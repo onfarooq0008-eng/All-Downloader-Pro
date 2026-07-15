b29794289c142383547b8db775c247ece6d0c968
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-15 10:57:33 +0400

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -4,42 +4,93 @@ const router=express.Router();
 
 const ytdlp=require("yt-dlp-exec");
 
+const {
+downloadVideo,
+downloadAudio
+}=require("../services/downloader");
+
+
+// Video Information
+
 router.post("/",async(req,res)=>{
 
 try{
 
 const url=req.body.url;
 
-if(!url){
-
-return res.status(400).send("URL Required");
-
-}
 
 const info=await ytdlp(url,{
-
 dumpSingleJson:true,
-
-noWarnings:true,
-
-noCheckCertificates:true
-
+noWarnings:true
 });
+
 
 res.render("pages/result",{
-
 info
-
 });
 
-}catch(err){
 
-console.log(err);
+}catch(error){
 
-res.status(500).send("Download Error");
+console.log(error);
+
+res.send("Invalid URL");
 
 }
 
 });
+
+
+
+// Video Download
+
+router.get("/video",async(req,res)=>{
+
+try{
+
+const file=await downloadVideo(
+req.query.url
+);
+
+
+res.download(file);
+
+
+}catch(error){
+
+console.log(error);
+
+res.send("Video download failed");
+
+}
+
+});
+
+
+
+// MP3 Download
+
+router.get("/audio",async(req,res)=>{
+
+try{
+
+const file=await downloadAudio(
+req.query.url
+);
+
+
+res.download(file);
+
+
+}catch(error){
+
+console.log(error);
+
+res.send("Audio download failed");
+
+}
+
+});
+
 
 module.exports=router;
